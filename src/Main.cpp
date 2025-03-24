47cd579c4b2be8e352c223c6ea73f6adfc109e83
ibrahimamam1 <ibrahima59@iut-dhaka.edu>
2025-03-25 01:10:21 +0600

    decode list

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,8 @@
 #include <cctype>
 #include <cstdlib>
 #include <iostream>
+#include <stack>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -32,12 +34,69 @@ json decode_int(const std::string &encoded_value) {
   }
 }
 
+std::string decode_list(const std::string &encode_value) {
+  std::cerr << "Decoding list\n";
+  std::string list_str = "[";
+  size_t len = encode_value.length();
+  size_t cursor = 1;
+  bool first = true;
+  std::stack<char> types_encountered;
+  types_encountered.push('l');
+  while (cursor < len) {
+    if (!first)
+      list_str += ",";
+    if (std::isdigit(encode_value[cursor])) {
+      std::cerr << "found a string\n";
+      first = false;
+      size_t colon_index = encode_value.find(':', cursor);
+      std::string number_str = encode_value.substr(cursor, colon_index);
+      size_t number = std::atoll(number_str.c_str());
+      json str = decode_string(encode_value.substr(cursor, cursor + number + 1));
+      list_str += str.dump();
+                cursor = colon_index + number + 1;
+
+    } else if (encode_value[cursor] == 'i') {
+      std::cerr << "found an int\n";
+      first = false;
+      size_t end_index = encode_value.find('e', cursor);
+      std::string number = decode_int(encode_value.substr(cursor, end_index));
+      list_str += number;
+      cursor = end_index + 1;
+    } else if (encode_value[cursor] == 'l') {
+      std::cerr << "found a list\n";
+      types_encountered.push('l');
+      list_str += "[";
+      first = true;
+      cursor++;
+    } else if (encode_value[cursor] == 'e') {
+      if (types_encountered.size() == 0)
+        throw std::runtime_error("Invalid bencode value: " + encode_value);
+      else if (types_encountered.top() == 'l')
+        list_str[list_str.length() - 1] = ']';
+      types_encountered.pop();
+      cursor++;
+    } else {
+      throw std::runtime_error("Invalid bencode value: " + encode_value);
+    }
+    std::cerr << "list: " << list_str << " \n";
+    std::cerr << "cursor: " << cursor << " \n";
+  }
+  if (types_encountered.size() != 0)
+    throw std::runtime_error("Invalid Bencode value: " + encode_value);
+
+  if (list_str.length() == 1)
+    list_str = "[]"; // special case of empty list
+  return list_str;
+}
+
 std::string decode_bencoded_value(const std::string &encoded_value) {
   if (std::isdigit(encoded_value[0])) {
     json decoded_str = decode_string(encoded_value);
     return decoded_str.dump();
   } else if (encoded_value[0] == 'i') {
     return decode_int(encoded_value);
+  } else if (encoded_value[0] == 'l') {
+    return decode_list(encoded_value);
   } else {
     throw std::runtime_error("Unhandled encoded value: " + encoded_value);
   }
